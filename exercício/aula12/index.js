c0ce07a7251e4417ca0311d03d72a6b433c1dd19
Davi Tavares Morais <davidemorais33@gmail.com>
2025-10-14 20:49:37 -0300

    oi

---
+++ exercício/aula12/index.js
@@ -0,0 +1,15 @@
+ var idade = 67;
+console.log(`Você tem ${idade} anos`);
+
+if (idade < 16) {
+    console.log('Não pode votar');
+} else {
+    console.log('Pode votar');
+    
+    if (idade < 18 || idade > 65) {
+        console.log('Voto opcional');
+    } else {
+        console.log('Voto obrigatório');
+    }
+}
+ 
