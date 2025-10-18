0291e31f31762015ebc80ed4ec6567837ae1843b
Davi Tavares Morais <davidemorais33@gmail.com>
2025-10-17 22:26:53 -0300

    oi

---
+++ atividades/ati5.js
@@ -0,0 +1,5 @@
+var vel = 30
+console.log(`A velocidade do seu carro é ${vel} k/h`)
+if(vel > 60){
+    console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
+}else{ console.log(`Dirija sempre usando cinto de segurança!`)}
