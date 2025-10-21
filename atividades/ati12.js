3b3df97c35ecad492140d9172b614a38429966a2
Davi Tavares Morais <davidemorais33@gmail.com>
2025-10-20 21:51:18 -0300

    f

---
+++ atividades/ati12.js
@@ -0,0 +1,17 @@
+var comu = 100
+var vip = 0
+var novo = 500
+
+var valos = 0
+
+if (vip > 500 || vip == 500){
+    console.log('20% de desconto');
+    
+} else if(vip < 500){
+
+     console.log('10% de desconto');
+     
+} else if(valos == 0 || valos < 0 ){
+
+    console.log('Valor inválido!');  
+} 
