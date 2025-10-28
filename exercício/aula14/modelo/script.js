e974e2a1d97f4a115908e92c5aa1e3796d7c097f
Davi Tavares Morais <davidemorais33@gmail.com>
2025-10-28 16:32:07 -0300

    d

--- exercício/aula14/modelo/script.js
+++ exercício/aula14/modelo/script.js
@@ -8,6 +8,36 @@ function variaficar(){
         window.alert('[ERRO] Variaficado os dados e tente novamente!')
     } else{
           var fsex = document.getElementsByName('redsex')
-          var idade = ano - Number()
+          var idade = ano - Number(fano.value)
+          var genero = ''
+          var img = document.createElement('ing')
+          img = setAttribute('id', 'foto')
+          if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('img', 'bebeh.png')
+            } else if (idade < 21){
+                //jovem
+            }(idade < 50){
+                //adulto
+            } else {
+                //idoso
+            }
+          } else if (fsex[1].checked){
+            genero= 'Mulher'
+            if(idade >=0 && idade < 10){
+                //criança
+            } else if (idade < 21){
+                //jovem
+            }(idade < 50){
+                //adulto
+            } else {
+                //idoso
+            }
+          }
+          //res.Style.textAlign='center'
+          res.style.textAlign = 'center';
+          res.innerHTML = `Detecamos ${genero} com ${idade} ano`
     }
 }
