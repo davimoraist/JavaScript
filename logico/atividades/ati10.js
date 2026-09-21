c281e5a094043b5716d987e28f6d919523a8f41c
Davi Tavares Morais <davidemorais33@gmail.com>
2026-09-21 11:29:05 -0300

    Create ati10.js

---
+++ logico/atividades/ati10.js
@@ -0,0 +1,7 @@
+//100-a=a/100=a*b=ab-c=d
+var salario = 1500
+var vendido = 5000
+var comisao = (100 - 4) / 100
+var soma = comisao*vendido
+var Total = vendido-soma
+console.log(`Total seu vendo é ${vendido} então seu comião vai sei ${Total+salario}`) 
